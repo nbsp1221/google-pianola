54604c34985a559d0fb51ce561b473a1dc9b9cf9
Naki <nbsp1221@gmail.com>
2021-10-16 07:38:47 +0900

    코난 백업 빼먹은 코드 업데이트

--- konan-06-op.js
+++ konan-06-op.js
@@ -1,3 +1,52 @@
+
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('1');
+    }, ms);
+  })
+}
+
+const clavierList = [];
+for(const level of  document.getElementById('piano').childNodes[5].shadowRoot.getElementById('container').getElementsByTagName('piano-keyboard-octave')) {
+  const aList = [];
+  const bList = [];
+  
+  for(const clavier of  level.shadowRoot.getElementById('white-notes').getElementsByTagName('piano-keyboard-note')) {
+    const c = clavier.shadowRoot.getElementById('container').getElementsByTagName('button')[0];
+    aList.push(c);
+  }
+
+  for(const clavier of  level.shadowRoot.getElementById('black-notes').getElementsByTagName('piano-keyboard-note')) {
+    const c = clavier.shadowRoot.getElementById('container').getElementsByTagName('button')[0];
+    bList.push(c);
+  }
+  bList.shift();
+
+  clavierList.push({
+    'low': aList,
+    'high': bList,
+  });
+}
+
+/*
+  도 = 1
+  레 = 2
+  미 = 3
+  파 = 4
+  솔 = 5
+  라 = 6
+  시 = 7
+*/
+
+async function send(level, idx, isHigh=false, _delay=100) {
+  const blackWhite = isHigh ? 'high' : 'low';
+
+  clavierList[level][blackWhite][idx-1].dispatchEvent(new Event('mousedown'));
+  await delay(_delay);
+  clavierList[level][blackWhite][idx-1].dispatchEvent(new Event('mouseup'));
+}
+
 const unit = 1200;
 let count = 0;
 
